e73e718d113433db1494c310e7d97bcba196fd4e
vladim1r1337 <vladim1r.gribkov@yandex.ru>
2023-08-20 15:29:22 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h>
+        Здесь скоро будет Skypro.Music
+        </h>
+      </header>
+    </div>
+  );
+}
+
+export default App;
