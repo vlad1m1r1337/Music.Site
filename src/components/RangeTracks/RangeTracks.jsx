0d0f4aa5ffd1c89ad9554b1e0dab0e33bd37143e
Vlad1m1r1337 <vladim1r.gribkov@yandex.ru>
2024-03-24 17:27:13 +0400

    processing new popup

--- src/components/RangeTracks/RangeTracks.jsx
+++ src/components/RangeTracks/RangeTracks.jsx
@@ -1,4 +1,3 @@
-// Ваш родительский компонент
 import SearchPopup from '../SearchPopup/SearchPopup.jsx';
 import * as S from './RangeTracks.styles'
 import {PopupFilter} from "../../services/constants";
@@ -7,13 +6,14 @@ export default function RangeTracks() {
 
   return (
     <>
-      <S.CenterBlockH2>Треки</S.CenterBlockH2>
-      <S.CenterBlockFilter>
-        <S.FilterTitle>Искать по:</S.FilterTitle>
-        <SearchPopup name={PopupFilter[0]} />
-        <SearchPopup name={PopupFilter[1]} />
-        <SearchPopup name={PopupFilter[2]} />
-      </S.CenterBlockFilter>
+        <S.CenterBlockH2>Треки</S.CenterBlockH2>
+            <S.CenterBlockFilter>
+            <S.FilterTitle>Искать по:</S.FilterTitle>
+            <SearchPopup name={PopupFilter[0]} />
+            <SearchPopup name={PopupFilter[1]} />
+            <p>Сортировка: </p>
+            <SearchPopup name={"По умолчанию"} />
+        </S.CenterBlockFilter>
     </>
   );
 }
